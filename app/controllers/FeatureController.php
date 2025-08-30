c2960866fc41f17c00f0e2d51a36890d3eaca73f
Alojamientos-Morona <alojamientosmorona@gmail.com>
2025-08-30 13:44:06 -0500

    Add editable feature section with icons

---
+++ app/controllers/FeatureController.php
@@ -0,0 +1,69 @@
+<?php
+require_once __DIR__ . '/../models/Feature.php';
+
+class FeatureController
+{
+    public function handle(): void
+    {
+        session_start();
+        if (!isset($_SESSION['username'])) {
+            header('Location: inicio.php');
+            exit;
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $action = $_POST['action'] ?? '';
+            switch ($action) {
+                case 'create':
+                    $title = $_POST['title'] ?? '';
+                    $description = $_POST['description'] ?? '';
+                    $icon = '';
+                    if (isset($_FILES['icon']) && $_FILES['icon']['error'] === UPLOAD_ERR_OK) {
+                        $uploadDir = __DIR__ . '/../../assets/images/icons/';
+                        if (!is_dir($uploadDir)) {
+                            mkdir($uploadDir, 0777, true);
+                        }
+                        $filename = time() . '-' . basename($_FILES['icon']['name']);
+                        $destination = $uploadDir . $filename;
+                        if (move_uploaded_file($_FILES['icon']['tmp_name'], $destination)) {
+                            $icon = 'assets/images/icons/' . $filename;
+                        }
+                    }
+                    if ($title && $description && $icon) {
+                        Feature::create($title, $description, $icon);
+                    }
+                    break;
+                case 'update':
+                    $id = isset($_POST['id']) ? (int)$_POST['id'] : 0;
+                    $title = $_POST['title'] ?? '';
+                    $description = $_POST['description'] ?? '';
+                    $icon = $_POST['current_icon'] ?? '';
+                    if (isset($_FILES['icon']) && $_FILES['icon']['error'] === UPLOAD_ERR_OK) {
+                        $uploadDir = __DIR__ . '/../../assets/images/icons/';
+                        if (!is_dir($uploadDir)) {
+                            mkdir($uploadDir, 0777, true);
+                        }
+                        $filename = time() . '-' . basename($_FILES['icon']['name']);
+                        $destination = $uploadDir . $filename;
+                        if (move_uploaded_file($_FILES['icon']['tmp_name'], $destination)) {
+                            $icon = 'assets/images/icons/' . $filename;
+                        }
+                    }
+                    if ($id && $title && $description && $icon) {
+                        Feature::update($id, $title, $description, $icon);
+                    }
+                    break;
+                case 'delete':
+                    $id = isset($_POST['id']) ? (int)$_POST['id'] : 0;
+                    if ($id) {
+                        Feature::delete($id);
+                    }
+                    break;
+            }
+        }
+
+        header('Location: index.php');
+        exit;
+    }
+}
+?>
---
+++ features.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . '/app/controllers/FeatureController.php';
+
+$controller = new FeatureController();
+$controller->handle();
+?>
