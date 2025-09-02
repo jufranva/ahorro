6c7a82c21c242a0e405213a39a8926118fe212f7
Alojamientos-Morona <alojamientosmorona@gmail.com>
2025-09-01 20:23:10 -0500

    feat: add recent arrivals section on home

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,17 +1,27 @@
 <?php
-require_once __DIR__ . '/../models/Product.php';
 require_once __DIR__ . '/../models/Slide.php';
 require_once __DIR__ . '/../models/Banner.php';
 require_once __DIR__ . '/../models/Feature.php';
+require_once __DIR__ . '/../models/Garment.php';
+require_once __DIR__ . '/../models/State.php';
 
 class HomeController
 {
     public function index(): void
     {
-        $products = Product::all();
         $slides = Slide::all();
         $banners = Banner::all();
         $features = Feature::all();
+
+        $stateId = State::getIdByName('Recien llegado');
+        $garments = $stateId === null ? [] : Garment::all(null, $stateId, null, 'new');
+        $newArrivals = array_values(array_filter($garments, function ($g) {
+            return ($g['type'] ?? '') === 'nueva';
+        }));
+        $usedArrivals = array_values(array_filter($garments, function ($g) {
+            return ($g['type'] ?? '') === 'usada';
+        }));
+
         include __DIR__ . '/../views/home.php';
     }
 }
