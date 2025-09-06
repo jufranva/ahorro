60db847d5389a4900526124a961c02cd96691fe1
Alojamientos-Morona <alojamientosmorona@gmail.com>
2025-09-06 16:54:36 -0500

    Remove cart item subtotal column

--- app/views/cart.php
+++ app/views/cart.php
@@ -6,55 +6,55 @@
     <?php else: ?>
       <div class="row">
         <div class="col-12">
-          <div class="cart-table table-responsive">
-            <table class="table table-bordered">
-              <thead>
-                <tr>
-                  <th class="pro-thumbnail">Imagen</th>
-                  <th class="pro-title">Producto</th>
-                  <th class="pro-price">Precio</th>
-                  <th class="pro-quantity">Cantidad</th>
-                  <th class="pro-subtotal">Sub Total</th>
-                  <th class="pro-remove">Eliminar</th>
-                </tr>
-              </thead>
-              <tbody>
-                <?php foreach ($items as $item): ?>
+          <div class="cart-table">
+            <div class="table-responsive">
+              <table class="table table-bordered">
+                <thead>
                   <tr>
-                    <td class="pro-thumbnail">
-                      <?php
-                        $basePath = __DIR__ . '/../../';
-                        $img = $item['image_primary'] ?? '';
-                        $imgSrc = '';
-                        if ($img && (filter_var($img, FILTER_VALIDATE_URL) || is_file($basePath . $img))) {
-                          $imgSrc = asset($img);
-                        }
-                      ?>
-                      <?php if ($imgSrc): ?>
-                        <a href="<?= htmlspecialchars(asset('prenda.php?id=' . (int)$item['garment_id']), ENT_QUOTES, 'UTF-8'); ?>">
-                          <img class="img-fluid" src="<?= htmlspecialchars($imgSrc, ENT_QUOTES, 'UTF-8'); ?>" alt="<?= htmlspecialchars($item['name'], ENT_QUOTES, 'UTF-8'); ?>">
-                        </a>
-                      <?php endif; ?>
-                    </td>
-                    <td class="pro-title">
-                      <a href="<?= htmlspecialchars(asset('prenda.php?id=' . (int)$item['garment_id']), ENT_QUOTES, 'UTF-8'); ?>">
-                        <?= htmlspecialchars($item['name'], ENT_QUOTES, 'UTF-8'); ?>
-                      </a>
-                    </td>
-                    <td class="pro-price"><span>$<?= number_format((float)$item['sale_value'], 2); ?></span></td>
-                    <td class="pro-quantity"><span><?= (int)$item['quantity']; ?></span></td>
-                    <td class="pro-subtotal"><span>$<?= number_format((float)$item['sale_value'] * $item['quantity'], 2); ?></span></td>
-                    <td class="pro-remove">
-                      <form method="post" action="<?= htmlspecialchars(asset('cart.php'), ENT_QUOTES, 'UTF-8'); ?>">
-                        <input type="hidden" name="action" value="remove">
-                        <input type="hidden" name="id" value="<?= (int)$item['id']; ?>">
-                        <button type="submit" class="btn btn-link p-0"><i class="pe-7s-trash"></i></button>
-                      </form>
-                    </td>
+                    <th class="pro-thumbnail">Imagen</th>
+                    <th class="pro-title">Producto</th>
+                    <th class="pro-price">Precio</th>
+                    <th class="pro-quantity">Cantidad</th>
+                    <th class="pro-remove">Eliminar</th>
                   </tr>
-                <?php endforeach; ?>
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  <?php foreach ($items as $item): ?>
+                    <tr>
+                      <td class="pro-thumbnail">
+                        <?php
+                          $basePath = __DIR__ . '/../../';
+                          $img = $item['image_primary'] ?? '';
+                          $imgSrc = '';
+                          if ($img && (filter_var($img, FILTER_VALIDATE_URL) || is_file($basePath . $img))) {
+                            $imgSrc = asset($img);
+                          }
+                        ?>
+                        <?php if ($imgSrc): ?>
+                          <a href="<?= htmlspecialchars(asset('prenda.php?id=' . (int)$item['garment_id']), ENT_QUOTES, 'UTF-8'); ?>">
+                            <img class="img-fluid" src="<?= htmlspecialchars($imgSrc, ENT_QUOTES, 'UTF-8'); ?>" alt="<?= htmlspecialchars($item['name'], ENT_QUOTES, 'UTF-8'); ?>">
+                          </a>
+                        <?php endif; ?>
+                      </td>
+                      <td class="pro-title">
+                        <a href="<?= htmlspecialchars(asset('prenda.php?id=' . (int)$item['garment_id']), ENT_QUOTES, 'UTF-8'); ?>">
+                          <?= htmlspecialchars($item['name'], ENT_QUOTES, 'UTF-8'); ?>
+                        </a>
+                      </td>
+                      <td class="pro-price"><span>$<?= number_format((float)$item['sale_value'], 2); ?></span></td>
+                      <td class="pro-quantity"><span><?= (int)$item['quantity']; ?></span></td>
+                      <td class="pro-remove">
+                        <form method="post" action="<?= htmlspecialchars(asset('cart.php'), ENT_QUOTES, 'UTF-8'); ?>">
+                          <input type="hidden" name="action" value="remove">
+                          <input type="hidden" name="id" value="<?= (int)$item['id']; ?>">
+                          <button type="submit" class="btn btn-link p-0"><i class="pe-7s-trash"></i></button>
+                        </form>
+                      </td>
+                    </tr>
+                  <?php endforeach; ?>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
