548dea9cccedbf54595e0082a8ee5ad333655136
jufranva <jufranva@gmail.com>
2025-09-14 14:43:44 -0500

    pagina de agracedimiento

--- app/views/gracias.php
+++ app/views/gracias.php
@@ -3,10 +3,10 @@
     <div class="container my-5">
         <div class="row justify-content-center">
             <div class="col-lg-8 text-center">
-                <h1 class="mb-4">🎉 <span style="font-family: 'Brush Script MT', 'Lucida Handwriting', cursive;">¡Gracias por tu compra!</span> 🎉</h1>
+                <h1 class="mb-4">🎉 <span style="font-family: 'Brush Script MT', 'Lucida Handwriting', cursive; font-size: 170%;">¡Gracias por tu compra!</span> 🎉</h1>
                 <p>Hemos recibido tu pedido correctamente.</p>
                 <p>🕘 Esta noche nos pondremos en contacto al número telefónico que nos proporcionaste para coordinar el pago y el envío de tus productos.</p>
-                <p>💛 ¡Gracias por comprar en EL ARMARIO DEL AHORRO!</p>
+                <p>❤️ ¡Gracias por comprar en <b>EL ARMARIO DEL AHORRO!</b></p>
             </div>
         </div>
     </div>
